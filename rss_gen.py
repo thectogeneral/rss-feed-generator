176c37576d9ed3b57d91a1e624cd7d2b17446e45
thectogeneral <salemolorundare@gmail.com>
2024-08-29 00:23:18 +0100

    get doc from url

--- rss_gen.py
+++ rss_gen.py
@@ -5,7 +5,20 @@ import os
 app = Flask(__name__)
 app.config["CACHE_TYPE"] = os.environ.get("CACHE_TYPE", "SimpleCache")
 
+class ChannelNotFound(Exception):
+    pass
+
+async def get_doc_from_url(url):
+    try:
+        async with httpx.AsyncClient() as client:
+            res = await client.get(url)
+            res.raise_for_status()
+        return BeautifulSoup(res.content, "lxml")
+    except httpx.HTTPStatusError as e:
+        if "Redirect response" in str(e):
+            raise ChannelNotFound()
 
 
 async def channel_to_rss(channel):
-    pass
+    url = f"https://t.me/s/{channel}"
+    doc = await get_doc_from_url(url)
